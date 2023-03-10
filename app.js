a3f9d58a6ab1dbd62b5d96549cedbb9f34d6140d
MicahD18 <micah.daise@nwktc.edu>
2023-03-09 17:36:14 -0700

    added code that generates content for app

--- app.js
+++ app.js
@@ -18,29 +18,50 @@ async function generateApp() {
 
     const appPath = path.join(__dirname, appName.name);
 
-    const appFolders = [
-      "server",
-      "models",
-      "views",
-      "controllers",
-      "assets",
-      "styles",
-    ];
+    const appFolders = ["models", "views", "controllers", "assets", "styles"];
+    const viewFiles = ["index.html", "404.html"];
+    const styleFiles = ["index.css", "404.css"];
 
     fs.mkdir(appPath, { recursive: true }, (err) => {
       if (err) throw err;
-      appFolders.forEach((folderName) => {
-        fs.mkdir(`${appPath}/${folderName}`, { recursive: true }, (err) => {
-          if (err) {
-            console.error("Error generating app folders", err);
-          }
-          console.log(`Folder ${folderName} created successfully`);
-        });
-      });
+
+      generateAppContent(appPath, appFolders, viewFiles, styleFiles);
     });
   } catch (err) {
     console.log("something went wrong", err);
   }
+}
+
+function generateAppContent(appPath, appFolders, viewFiles, styleFiles) {
+  appFolders.forEach((folderName) => {
+    fs.mkdir(`${appPath}/${folderName}`, { recursive: true }, (err) => {
+      if (err) {
+        console.error("Error generating app folders", err);
+      }
+      if (folderName === "views") {
+        viewFiles.forEach((file) => {
+          fs.writeFileSync(
+            `${appPath}/views/${file}`,
+            "/* generate html snippet */\ndoc"
+          );
+        });
+      }
+
+      if (folderName === "styles") {
+        styleFiles.forEach((file) => {
+          fs.writeFileSync(`${appPath}/styles/${file}`, "");
+        });
+      }
+
+      // write folder file
+      fs.writeFileSync(
+        `${appPath}/server.js`,
+        "// simple express server snippet:\ness"
+      );
+
+      console.log(`Folder ${folderName} created successfully`);
+    });
+  });
 }
 
 generateApp();
